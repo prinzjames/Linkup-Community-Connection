eaa591814a14667f9fdb4934f8183275c5007037
prinzjames <princemwangi75@gmail.com>
2026-05-12 12:27:44 -0700

    fix mongoose duplicate model registration

--- backend/models/post.js
+++ backend/models/post.js
@@ -45,4 +45,4 @@ const postSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-module.exports = mongoose.model("Post", postSchema);
+module.exports = mongoose.models.Post || mongoose.model("Post", postSchema);
